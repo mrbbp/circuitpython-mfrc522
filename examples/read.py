11355be2433d050863d038b61d7c2a6b9dbd1b7d
mrbbp <eric@mrbbp.com>
2023-12-07 23:00:51 +0100

    Update read.py
    fonctionnel sur seed xiao

--- examples/read.py
+++ examples/read.py
@@ -1,49 +1,56 @@
 """
 Exemple de lecture de tag avec un module ``mfrc522``.
 """
-
-# 3rd party
-import board
-
-# this package
 import mfrc522
+import board
+import time
+from os import uname
+import digitalio
 
+led = digitalio.DigitalInOut(board.LED)
+led.direction = digitalio.Direction.OUTPUT
+led.value = True
 
 def do_read():
-        # modified for rp2040 - XIAO
-	rdr = mfrc522.MFRC522(board.SCK, board.MOSI, board.MISO, cs=board.D7, rst=board.D6)
-	rdr.set_antenna_gain(0x07 << 4)
 
-	print('')
-	print("Posez un tag sur le lecteur pour lire les données")
-	print('')
+  if uname()[0] == 'rp2040':
+    # rdr = mfrc522.MFRC522(sck=board.SCK, mosi=board.MOSI, miso=board.MISO, cs=board.D9, rst=board.D8)
+    rdr = mfrc522.MFRC522(sck=board.SCK, mosi=board.MOSI, miso=board.MISO, cs=board.D7, rst=board.D6)
+  else:
+    raise RuntimeError("Unsupported platform")
 
-	try:
-		while True:
+  print("")
+  print("Placez une carte sur le lecteur")
+  print("")
 
-			(stat, tag_type) = rdr.request(rdr.REQIDL)
+  try:
+    while True:
+      (stat, tag_type) = rdr.request(rdr.REQIDL)
 
-			if stat == rdr.OK:
+      if stat == rdr.OK:
+        (stat, raw_uid) = rdr.anticoll()
+        if stat == rdr.OK:
+          led.value = False
+          print("Nouveau tag détecté")
+          print("  - type de tag : 0x%02x" % tag_type)
+          print("  - uid   : 0x%02x%02x%02x%02x" % (raw_uid[0], raw_uid[1], raw_uid[2], raw_uid[3]))
+          print("")
 
-				(stat, raw_uid) = rdr.anticoll()
+          if rdr.select_tag(raw_uid) == rdr.OK:
 
-				if stat == rdr.OK:
-					print("Tag détecté")
-					print("  - Type de tag : 0x%02x" % tag_type)
-					print("  - uid\t : 0x%02x%02x%02x%02x" % (raw_uid[0], raw_uid[1], raw_uid[2], raw_uid[3]))
-					print('')
+            key = [0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF]
 
-					if rdr.select_tag(raw_uid) == rdr.OK:
+            if rdr.auth(rdr.AUTHENT1A, 8, key, raw_uid) == rdr.OK:
+              print("Address 8 data: %s" % rdr.read(8))
+              rdr.stop_crypto1()
+            else:
+              print("Pas d'Authentication")
+          else:
+            print("sélection erronée du tag")
+            
+          #time.sleep(.5)
+          led.value = True
+          
+  except KeyboardInterrupt:
+      print("Aurevoir")
 
-						key = [0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF]
-
-						if rdr.auth(rdr.AUTHENT1A, 8, key, raw_uid) == rdr.OK:
-							print("Données (Address 8 data) : %s" % rdr.read(8))
-							rdr.stop_crypto1()
-						else:
-							print("Erreur d'authentication")
-					else:
-						print("Selection du tag éronnée")
-
-	except KeyboardInterrupt:
-		print("Au revoir")
